8d16e0847bbc2b59b3a1bcc5eb12bac64a7ddd82
Melina Carniel <Melina.Carniel@targetsistemas.com.br>
2019-04-08 17:52:55 -0300

    fist commit

---
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewContainerRef } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
+import { ModalDialogService } from 'nativescript-angular/modal-dialog';
+import { DayModalComponent } from '../day-modal/day-modal.component';
+import { UIService } from '~/app/shared/ui.service';
+
+@Component({
+  selector: 'ns-current-challenge',
+  templateUrl: './current-challenge.component.html',
+  styleUrls: ['./current-challenge.component.css'],
+  moduleId: module.id
+})
+export class CurrentChallengeComponent {
+  constructor(private router: RouterExtensions,
+              private modalDialog: ModalDialogService,
+              private vcRef: ViewContainerRef,
+              private uiService: UIService) {}
+
+  // onEdit() {
+  //   this.router.navigate(['/challenges/edit'], {
+  //     transition: { name: 'slideLeft' }
+  //   });
+  // }
+
+  onChangeStatus(){
+    this.modalDialog.showModal(DayModalComponent, {
+      fullscreen: true,
+      viewContainerRef: this.uiService.getRootVCRef() ? this.uiService.getRootVCRef() : this.vcRef,
+      context: {date: new Date()}
+    }).then((result: string) => {
+      console.log(result)
+    });
+  }
+}
